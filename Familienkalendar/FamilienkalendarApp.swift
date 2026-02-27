ebcc9b5aa6976951ccf664dc569bbf70e881f1fd
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2026-02-27 13:12:48 +0100

    Initial Commit

---
+++ Familienkalendar/FamilienkalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilienkalendarApp.swift
+//  Familienkalendar
+//
+//  Created by Matthias Wallner-Géhri on 27.02.26.
+//
+
+import SwiftUI
+
+@main
+struct FamilienkalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
